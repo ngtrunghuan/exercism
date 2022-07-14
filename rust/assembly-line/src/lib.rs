c188392a8ce9bd1a93f54fa53a0be9eddbb40999
Huan Nguyen <ngtrunghuan@gmail.com>
2022-07-15 00:36:12 +0800

    add old; finish assembly-line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,16 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    221.0
+        * (speed as f64)
+        * match speed {
+            0 => 0.0,
+            1..=4 => 1.0,
+            5..=8 => 0.9,
+            _ => 0.77,
+        }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
